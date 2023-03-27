6c157e1cd56c4c4761c28a7097c456e7dd085e32
httpsoxfonchpemnetdir <128884574+httpsoxfonchpemnetdir@users.noreply.github.com>
2023-03-27 04:16:44 -0600

    Update Openssh.js

--- Openssh.js
+++ Openssh.js
@@ -1,23 +1,24 @@
-$ ssh-keygen -t ed25519 -C "hectorglezleon@gmail.com"
+$ ssh-keygen -t ed25519 -C "httpsoxfonchpemnetdir"
 > Generating public/private ALGORITHM key pair.
 > Enter a file in which to save the key (/home/YOU/.ssh/ALGORITHM):Enter
-> Enter passphrase (empty for no passphrase): [Now sky si open]
-> Enter same passphrase again: [Now sky si open]
+> Enter passphrase (empty for no passphrase): Enter
+> Enter same passphrase again: Enter
 
 $ eval "$(ssh-agent -s)"
 > Agent pid 59566
+
 $ ssh-add ~/.ssh/id_ed25519
 
-$ ssh-keygen -t ed25519-sk -C "httpsoxfonchpemnetdir"
+$ ssh-keygen -t ed25519-sk -C "hectorglezleon@gmail.com"
 > Enter a file in which to save the key (/home/YOU/.ssh/id_ed25519_sk):Enter
-> Enter passphrase (empty for no passphrase): [Now sky is open]
-> Enter same passphrase again: [Now sky si open]
+> Enter passphrase (empty for no passphrase): Enter
+> Enter same passphrase again: Enter
 
 $ ssh-keygen -p -f ~/.ssh/id_ed25519
-> Enter old passphrase: [Now sky is open]
-> Key has comment 'hectorglezleon@gmail.com'
-> Enter new passphrase (empty for no passphrase): [Sky is open]
-> Enter same passphrase again: [Sky is open]
+> Enter old passphrase: Enter
+> Key has comment 'Enter'
+> Enter new passphrase (empty for no passphrase): Enter
+> Enter same passphrase again: Enter
 > Your identification has been saved with the new passphrase.
 
 env=~/.ssh/agent.env
@@ -46,11 +47,11 @@ ssh-add~/path/to/my/key
 
 > Initializing new SSH agent...
 > succeeded
-> Enter passphrase for /c/Users/YOU/.ssh/id_rsa:
+> Enter passphrase for /c/Users/YOU/.ssh/id_rsa: Enter
 > Identity added: /c/Users/YOU/.ssh/id_rsa (/c/Users/YOU/.ssh/id_rsa)
 > Welcome to Git (version 1.6.0.2-preview20080923)
 >
 > Run 'git help git' to display the help index.
 > Run 'git help ' to display help for specific commands.
 
-$ Open .ssh-keygen
+$ Open ssh
